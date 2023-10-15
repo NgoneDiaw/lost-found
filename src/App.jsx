2d884a61b17734a7afb65ed77f4660eaa6c36edd
yankee <yankeesuprem@gmail.com>
2023-10-15 21:47:56 +0000

    Init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from 'primereact/button'
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      Yeet Got the juice
+      <Button label='Got the juice' />
+    </div>
+  )
+}
